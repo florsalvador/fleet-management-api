9e5d00c12ee752ea98942eaebaa0fb69270d90e3
florsalvador <florsalvador219@gmail.com>
2024-05-20 03:17:17 -0500

    Se modifica la funcion get_trajectories para que muestre todas las ubicaciones de un taxi dada una fecha para el endpoint trajectories/taxi_id

--- app/app.py
+++ app/app.py
@@ -24,12 +24,32 @@ def main():
             taxi_info = {"id": taxi.id, "plate": taxi.plate}
             response.append(taxi_info)
         return jsonify(response)
-
+    
     @app.route("/trajectories/<int:taxi_id>", methods=["GET"])
     def get_trajectories(taxi_id):
-        """Get trajectories of a taxi for a specific date"""
+        """Get all the locations of a taxi for a specific date"""
         date = request.args.get("date")
         date_to_use = datetime.strptime(date, "%Y-%m-%d").date()
+        trajectories_query = Trajectories.query.filter(
+            Trajectories.taxi_id == taxi_id, func.date(Trajectories.date) == date_to_use
+        ).all()
+        response = []
+        for t in trajectories_query:
+            trajectory = {
+                "id": t.id,
+                "taxi_id": t.taxi_id,
+                "date": t.date,
+                "latitude": t.latitude,
+                "longitude": t.longitude,
+            }
+            response.append(trajectory)
+        return jsonify(response)
+
+    @app.route("/last-location/latest", methods=["GET"])
+    def get_last_location(taxi_id):
+        """Get last location of every taxi"""
+        date = request.args.get("date")
+        date_to_use = datetime.strptime(date, "%Y-%m-%d").date() # No necesita date ni taxi_id, corregir
         trajectories_query = Trajectories.query.filter(
             Trajectories.taxi_id == taxi_id, func.date(Trajectories.date) == date_to_use
         ).order_by(desc(Trajectories.id)).first()
