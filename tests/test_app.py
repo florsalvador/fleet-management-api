54c41beddfc3f635b5b3f7d99379b2646cfa3cf1
florsalvador <florsalvador219@gmail.com>
2024-05-21 01:57:17 -0500

    Se modifica test_app.py para remover el patch de uno de los tests ya que no estaba mockeando correctamente

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
 """"..."""
 import json
-from unittest.mock import patch
+# from unittest.mock import patch
 from .mock_data import taxis_response
 
 
@@ -10,10 +10,8 @@ def test_get_taxis(client):
     assert response.status == "200 OK"
 
 
-@patch("app.models.Taxis", name="mock_taxis")
-def test_get_taxis_response(mock_taxis, client):
-    """Test for get_taxis using a mock to verify the response"""
-    mock_taxis.query.Paginate.return_value = taxis_response
+def test_get_taxis_response(client):
+    """Test for get_taxis to verify that the response is correct"""
     response = client.get("/taxis")
     assert response.status == "200 OK"
     assert json.loads(response.get_data()) == taxis_response
@@ -26,14 +24,14 @@ def test_get_trajectories_by_taxi(client):
 
 
 def test_get_trajectories_with_date(client):
-    """Test for get_trajectories(taxi_id) with date"""
+    """Test for get_trajectories(taxi_id) with date to verify status and length of data"""
     response = client.get("/trajectories/7088?date=2008-02-02")
     assert response.status == "200 OK"
     assert len(json.loads(response.get_data())) == 135
 
 
 def test_get_last_location(client):
-    """Test for get_last_location"""
+    """Test for get_last_location to verify status, length of data and the existence of plate"""
     response = client.get("/trajectories/latest")
     data = json.loads(response.get_data())
     assert response.status == "200 OK"
@@ -42,10 +40,3 @@ def test_get_last_location(client):
         assert "plate" in element
 
 
-# --------TEST_GET_TAXIS SIN MOCK
-
-# def test_get_taxis_response(client):
-#     """Test for get_taxis"""
-#     response = client.get("/taxis")
-#     assert response.status == "200 OK"
-#     assert json.loads(response.get_data()) == taxis_response
